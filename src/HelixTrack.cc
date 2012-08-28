530f7407895630a712750e48a87600733bb90ff4
Steven Aplin <steven.aplin@desy.de>
2012-08-28 10:22:12 +0000

    corrected spelling mistake in debug output.

--- src/HelixTrack.cc
+++ src/HelixTrack.cc
@@ -17,7 +17,7 @@ HelixTrack::HelixTrack( const double* x1, const double* x2, const double* x3, do
   TVector3 p2( x2[0], x2[1], x2[2] );
   TVector3 p3( x3[0], x3[1], x3[2] );
 
-  streamlog_out(DEBUG2) << "HelixTrack::HelixTrack Create from his: \n " 
+  streamlog_out(DEBUG2) << "HelixTrack::HelixTrack Create from hits: \n " 
   << "P1 x = " << p1.x() << " y = " << p1.y() << " z = " << p1.z() << " r = " << p1.Perp() << "\n " 
   << "P2 x = " << p2.x() << " y = " << p2.y() << " z = " << p2.z() << " r = " << p2.Perp() << "\n " 
   << "P3 x = " << p3.x() << " y = " << p3.y() << " z = " << p3.z() << " r = " << p3.Perp() << "\n "
