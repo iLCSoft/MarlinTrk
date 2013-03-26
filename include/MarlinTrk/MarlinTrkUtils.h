27f4179cd56b1b2211cbbc61cddffba0e38fdadf
Alexei Raspereza <oleksandr.volynets@desy.de>
2013-03-26 18:04:41 +0000

    Improved readability of the class

--- include/MarlinTrk/MarlinTrkUtils.h
+++ include/MarlinTrk/MarlinTrkUtils.h
@@ -29,11 +29,29 @@ namespace MarlinTrk{
 
 namespace MarlinTrk{
 
-  /** Takes a list of hits and uses the IMarlinTrack inferface to fit them using a supplied prefit containing a covariance matrix for the initialisation. The TrackImpl will have the 4 trackstates added to it @IP, @First_Hit, @Last_Hit and @CaloFace */
-  int createFinalisedLCIOTrack( IMarlinTrack* marlinTrk, std::vector<EVENT::TrackerHit*>& hit_list, IMPL::TrackImpl* track, bool fit_backwards, EVENT::TrackState* pre_fit, float bfield_z, double maxChi2Increment=DBL_MAX);
+  /** Takes a list of hits and uses the IMarlinTrack inferface to fit them using a supplied prefit containing
+   *  a covariance matrix for the initialisation. The TrackImpl will have the 4 trackstates added to
+   *  it @IP, @First_Hit, @Last_Hit and @CaloFace */
+  int createFinalisedLCIOTrack(
+      IMarlinTrack* marlinTrk,
+      std::vector<EVENT::TrackerHit*>& hit_list,
+      IMPL::TrackImpl* track,
+      bool fit_backwards,
+      EVENT::TrackState* pre_fit,
+      float bfield_z,
+      double maxChi2Increment=DBL_MAX);
   
-  /** Takes a list of hits and uses the IMarlinTrack inferface to fit them using a supplied covariance matrix for the initialisation. The TrackImpl will have the 4 trackstates added to it @IP, @First_Hit, @Last_Hit and @CaloFace */
-  int createFinalisedLCIOTrack( IMarlinTrack* marlinTrk, std::vector<EVENT::TrackerHit*>& hit_list, IMPL::TrackImpl* track, bool fit_backwards, const EVENT::FloatVec& initial_cov_for_prefit, float bfield_z, double maxChi2Increment=DBL_MAX);
+  /** Takes a list of hits and uses the IMarlinTrack inferface to fit them using a supplied covariance matrix
+   *  for the initialisation. The TrackImpl will have the 4 trackstates added to
+   *  it @IP, @First_Hit, @Last_Hit and @CaloFace */
+  int createFinalisedLCIOTrack(
+      IMarlinTrack* marlinTrk,
+      std::vector<EVENT::TrackerHit*>& hit_list,
+      IMPL::TrackImpl* track,
+      bool fit_backwards,
+      const EVENT::FloatVec& initial_cov_for_prefit,
+      float bfield_z,
+      double maxChi2Increment=DBL_MAX);
   
   /** Provides the values of a track state from the first, middle and last hits in the hit_list. */
   int createPrefit( std::vector<EVENT::TrackerHit*>& hit_list, IMPL::TrackStateImpl* pre_fit, float bfield_z, bool fit_backwards );
@@ -41,8 +59,17 @@ namespace MarlinTrk{
   /** Takes a list of hits and uses the IMarlinTrack inferface to fit them using a supplied prefit containing a covariance matrix for the initialisation. */  
   int createFit( std::vector<EVENT::TrackerHit*>& hit_list, IMarlinTrack* marlinTrk, EVENT::TrackState* pre_fit, float bfield_z, bool fit_backwards, double maxChi2Increment=DBL_MAX );
 
-  /** Takes a fitted MarlinTrack, TrackImpl to record the fit and the hits which have been added to the fit. The TrackImpl will have the 4 trackstates added to it @IP, @First_Hit, @Last_Hit and @CaloFace. Note: the hit list is needed as the IMarlinTrack only contains the hits used in the fit, not the spacepoints (if any have been included) so as the strip hits cannot point to the space points we need to have the list so that they can be recoreded in the LCIO TrackImpl */ 
-  int finaliseLCIOTrack( IMarlinTrack* marlinTrk, IMPL::TrackImpl* track, std::vector<EVENT::TrackerHit*>& hit_list, IMPL::TrackStateImpl* atLastHit=0, IMPL::TrackStateImpl* atCaloFace=0);
+  /** Takes a fitted MarlinTrack, TrackImpl to record the fit and the hits which have been added to the fit.
+   *  The TrackImpl will have the 4 trackstates added to it @IP, @First_Hit, @Last_Hit and @CaloFace.
+   *  Note: the hit list is needed as the IMarlinTrack only contains the hits used in the fit, not the spacepoints
+   *  (if any have been included) so as the strip hits cannot point to the space points we need to have the list so
+   *  that they can be recorded in the LCIO TrackImpl */
+  int finaliseLCIOTrack(
+      IMarlinTrack* marlinTrk,
+      IMPL::TrackImpl* track,
+      std::vector<EVENT::TrackerHit*>& hit_list,
+      IMPL::TrackStateImpl* atLastHit=0,
+      IMPL::TrackStateImpl* atCaloFace=0);
   
   /** Set the subdetector hit numbers for the TrackImpl */
   void addHitNumbersToTrack(IMPL::TrackImpl* track, std::vector<EVENT::TrackerHit*>& hit_list, bool hits_in_fit, UTIL::BitField64& cellID_encoder);
